f80fe9b3eb13f475306ee9a90d34dfa39b4f48c3
Jon Dewey <donjewey@gmail.com>
2022-05-31 19:01:01 -0700

    fixed lint errors

--- src/middleware/APIAuth.ts
+++ src/middleware/APIAuth.ts
@@ -13,21 +13,24 @@ import { Request, Response, NextFunction } from 'express';
 
 export default class AuthVerification {
     // Validates format of API requests
-    static endpointValidation(req: Request, res: Response, next: NextFunction): NextFunction {
-        const { project: projectID } = req.query;
+
+    public static endpointValidation(req: Request, res: Response, next: NextFunction) {
+        const { project } = req.query;
 
         // simplistic ID validation
-        if (projectID.length != 24) throw new Error('[Log API] URL invalid, check project ID');
+        if (project?.length !== 24) throw new Error('[Log API] URL invalid, check project ID');
 
         // if endpoint and IDs are valid
-        if (req.path.includes('log') && req.header.log_key.length == 10) {
+        if (req.path.includes('log') && req.headers.log_key?.length === 10) {
             return next();
-        } else throw new Error('[Log API] Endpoint and/or log_key header invalid');
+        }
+
+        throw new Error('[Log API] Endpoint and/or log_key header invalid');
     }
 
     // Verifies key provided in header matches key in associated project's entry in DB
-    static async keyVerification(req: Request, res: Response, next): NextFunction {
-        const { log_key: key } = req.header;
+    public static async keyVerification(req: Request, res: Response, next: NextFunction) {
+        const { log_key: key } = req.headers;
         const { project: projectID } = req.query;
 
         /*
@@ -36,7 +39,9 @@ export default class AuthVerification {
          * URI to gate web app must be included (once operational)
          */
         const gateURI = '';
-        const dbKey = await fetch(`${gateURI}/auth/${projectID}`);
+        const dbKey = await fetch(`${gateURI}/auth/${projectID}`).then((data): string =>
+            data.toString()
+        );
 
         if (key !== dbKey)
             throw new Error('[Log API] Header log_key incorrect, check key and project ID');
